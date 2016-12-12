c44cc2d5519c60af869c7476363fe424c8aadf4a
mars.wong <noneorone@yeah.net>
2016-12-12 20:24:57 +0800

    add build info

--- app/src/main/java/com/dmio/org/tut/activity/MainActivity.java
+++ app/src/main/java/com/dmio/org/tut/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.dmio.org.tut.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,11 +31,15 @@ import com.dmio.org.tut.activity.demo.widget.wheelpicker.WheelPickerActivity;
 import com.dmio.org.tut.activity.demo.security.ca.SecurityCAActivity;
 import com.dmio.org.tut.activity.guide.GuideMainActivity;
 
+import org.json.JSONStringer;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.lang.reflect.Field;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -41,6 +52,8 @@ import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private ComponentAdapter mComponentAdapter;
 
@@ -178,19 +191,21 @@ public class MainActivity extends AppCompatActivity {
                             if (pyAnrDir.exists()) {
                                 File[] dataAnrFiles = dataAnrDir.listFiles();
                                 if (dataAnrFiles != null) {
+                                    String deviceInfo = getDeviceInfo();
                                     for (int i = 0, len = dataAnrFiles.length; i < len; i++) {
                                         File dataAnrFile = dataAnrFiles[i];
                                         if (dataAnrFile != null) {
                                             File anrFile = new File(pyAnrDir, dataAnrFile.getName());
-                                            if (!anrFile.exists()) {
-                                                anrFile.createNewFile();
+                                            if (anrFile.exists()) {
+                                                anrFile.delete();
                                             }
+                                            anrFile.createNewFile();
                                             if (anrFile.exists()) {
                                                 BufferedReader br = new BufferedReader(new FileReader(dataAnrFile));
                                                 BufferedWriter bw = new BufferedWriter(new FileWriter(anrFile, true));
                                                 String line = null;
+                                                bw.write(deviceInfo);
                                                 while ((line = br.readLine()) != null) {
-                                                    Log.e(getClass().getName(), line);
                                                     bw.write(line.concat("\r\n"));
                                                 }
                                                 bw.close();
@@ -200,22 +215,76 @@ public class MainActivity extends AppCompatActivity {
                                         }
                                     }
 
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Toast.makeText(MainActivity.this, "anr files copyed", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
+                                    Looper.prepare();
+                                    Toast.makeText(MainActivity.this, "anr files copyed", Toast.LENGTH_SHORT).show();
+                                    Looper.loop();
                                 }
                             }
 
                         }
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    Log.e(TAG, "", e);
                 }
             }
         }).start();
     }
 
+    private String getDeviceInfo() {
+        StringBuilder info = new StringBuilder();
+
+        Context ctx = getBaseContext();
+        try {
+            PackageManager fields = ctx.getPackageManager();
+            PackageInfo pi = fields.getPackageInfo(ctx.getPackageName(), 0);
+            if (pi != null) {
+                String versionName = pi.versionName == null ? "null" : pi.versionName;
+                String versionCode = pi.versionCode + "";
+                info.append("versionCode=").append(versionCode).append("\n");
+                info.append("versionName=").append(versionName).append("\n");
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "", e);
+        }
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float scaledPixel = SizeUtils.convertSpToPixels(ctx, 100);
+        float deviceIndependent = SizeUtils.convertDpiToPixels(ctx, 100);
+        DecimalFormat dcmFmt = new DecimalFormat("0.0000");
+        info.append("resolution=").append(displayMetrics.widthPixels).append("x").append(displayMetrics.heightPixels).append(" (").append(ctx.getString(R.string.screenValue)).append(")\n");
+        info.append("densityDpi=").append(displayMetrics.densityDpi).append(" (").append(ctx.getString(R.string.dpiValue)).append(")\n");
+        info.append("density=").append(displayMetrics.density).append("\n");
+        info.append("scaledDensity=").append(displayMetrics.scaledDensity).append("\n");
+        info.append("xdpi=").append(displayMetrics.xdpi).append("\n");
+        info.append("ydpi=").append(displayMetrics.ydpi).append("\n");
+        info.append("fontScale=").append(dcmFmt.format(scaledPixel / deviceIndependent)).append("\n");
+        info.append("sw=").append(displayMetrics.widthPixels / displayMetrics.density).append("\n\n");
+
+        Field[] fields = Build.class.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                info.append(field.getName() + "=").append(field.get(field.getName())).append("\n");
+            } catch (IllegalAccessException e) {
+                Log.e(TAG, "", e);
+            }
+        }
+
+        return info.toString();
+    }
+
+}
+
+class SizeUtils {
+    public static float convertDpiToPixels(Context context, int dpi) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpi, context.getResources().getDisplayMetrics());
+    }
+
+    public static float convertSpToPixels(Context context, int sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+    public static float convertPtToPixels(Context context, int pt) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, pt, context.getResources().getDisplayMetrics());
+    }
 }
