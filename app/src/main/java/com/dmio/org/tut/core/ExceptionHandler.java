fe148f3bbde8416ab45856ddbe81ba7bf42e64d8
mars0122 <noneorone@yeah.net>
2017-04-01 17:48:08 +0800

    exception tip and process kill

--- app/src/main/java/com/dmio/org/tut/core/ExceptionHandler.java
+++ app/src/main/java/com/dmio/org/tut/core/ExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.dmio.org.tut.core;
 
+import android.os.Looper;
+import android.os.Process;
+import android.widget.Toast;
+
+import com.dmio.org.tut.application.ExApplication;
 import com.dmio.org.tut.core.log.Logger;
 
 /**
@@ -11,8 +16,37 @@ import com.dmio.org.tut.core.log.Logger;
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
 
     @Override
-    public void uncaughtException(Thread t, Throwable e) {
-        Logger.e(e);
+    public void uncaughtException(final Thread t, final Throwable th) {
+        // 记录异常日志
+        Logger.e(th);
+
+        // 弹出提示
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                Toast.makeText(ExApplication.getInstance().getBaseContext(), "Exception Occured!!!", Toast.LENGTH_LONG).show();
+                Looper.loop();
+            }
+        }) {
+        }.start();
+
+        // 延迟退出
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+                Looper.loop();
+            }
+        }) {
+        }.start();
     }
 
 }
