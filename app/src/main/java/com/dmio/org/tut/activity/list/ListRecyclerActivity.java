2f24e209749c38690cdac7c1855ce5817944d2f6
mars0122 <noneorone@yeah.net>
2017-04-15 14:58:51 +0800

    recycler view refresh

--- app/src/main/java/com/dmio/org/tut/activity/list/ListRecyclerActivity.java
+++ app/src/main/java/com/dmio/org/tut/activity/list/ListRecyclerActivity.java
@@ -1,17 +1,20 @@
 package com.dmio.org.tut.activity.list;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.dmio.org.tut.R;
+import com.dmio.org.tut.core.log.Logger;
 import com.dmio.org.tut.core.task.Task;
 import com.dmio.org.tut.core.utils.AssertUtils;
 import com.dmio.org.tut.core.utils.ViewUtils;
 import com.dmio.org.tut.data.model.Warrior;
-import com.dmio.org.tut.widget.sweetalert.SweetAlertDialog;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -20,86 +23,142 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 功能说明：带谷歌圆圈刷新样式的RecyclerView<br/>
+ * 功能说明：带刷新样式的RecyclerView<br/>
  * 作者：wangmeng on 2017/3/29 19:56<br/>
  * 邮箱：wangmeng@pycredit.cn
  */
-public class ListRecyclerActivity extends AppCompatActivity {
-
+public class ListRecyclerActivity extends AppCompatActivity implements Task.CallBack<List<Warrior>> {
 
     private RecyclerView mRvList;
-    private ListRefreshRecyclerViewAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private SwipeRefreshLayout cmpRefresh;
 
-    private Handler mHandler;
+    private List<Warrior> data;
+    private ListRecyclerAdapter mAdapter;
+
+    private boolean isLoading;
+    private int lastVisibleItemPosition;
+
+    private Handler handler;
 
     public Handler getHandler() {
-        if (mHandler == null) {
-            mHandler = new Handler();
+        if (handler == null) {
+            handler = new Handler();
         }
-        return mHandler;
+        return handler;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_refresh_recycler_view);
+        setContentView(R.layout.activity_list_recycler_view);
         initView();
-        initData();
     }
 
     private void initView() {
+        data = new ArrayList<>();
+        mAdapter = new ListRecyclerAdapter(this, data);
         mRvList = ViewUtils.get(this, R.id.rv_list);
-
         mRvList.setHasFixedSize(true);
-
-        mLayoutManager = new LinearLayoutManager(this);
-        mRvList.setLayoutManager(mLayoutManager);
-
-        List<Warrior> data = new ArrayList<>();
-        mAdapter = new ListRefreshRecyclerViewAdapter(this, data);
+        mRvList.setLayoutManager(new LinearLayoutManager(this));
+        mRvList.addItemDecoration(new VerticalLineDecorator(2));
         mRvList.setAdapter(mAdapter);
-    }
-
-    private void initData() {
-        final SweetAlertDialog dialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
-        dialog.setContentText("");
-        dialog.setTitleText("loading...");
-
-        Task.getInstance().exec(new Task.CallBack<List<Warrior>>() {
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRvList.setLayoutManager(layoutManager);
+        mRvList.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public void preExec() {
-                if (dialog != null && !dialog.isShowing()) {
-                    dialog.show();
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Logger.d("StateChanged = " + newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && lastVisibleItemPosition + 1 == mAdapter.getItemCount()) {
+                    Logger.d("loading executed");
+                    boolean isRefreshing = cmpRefresh.isRefreshing();
+                    if (isRefreshing) {
+                        mAdapter.notifyItemRemoved(mAdapter.getItemCount());
+                        return;
+                    }
+                    if (!isLoading) {
+                        isLoading = true;
+                        getHandler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Task.getInstance().exec(ListRecyclerActivity.this);
+                            }
+                        }, 1000L);
+                    }
                 }
             }
 
             @Override
-            public List<Warrior> inHandle() {
-                String jsonData = AssertUtils.getJsonData("list_recycler.json");
-                Gson gson = new Gson();
-                Type type = new TypeToken<List<Warrior>>() {
-                }.getType();
-                List<Warrior> list = gson.fromJson(jsonData, type);
-                return list;
-            }
-
-            @Override
-            public void complete(final List<Warrior> list) {
-                getHandler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (dialog != null) {
-                            dialog.dismissWithAnimation();
-                        }
-                        if (list != null && !list.isEmpty()) {
-                            mAdapter.setData(list);
-                            mAdapter.notifyDataSetChanged();
-                        }
-                    }
-                }, 1000L);
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Logger.d("onScrolled");
+                lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
             }
         });
+
+        cmpRefresh = ViewUtils.get(this, R.id.cmp_refresh);
+        cmpRefresh.setColorSchemeResources(R.color.c1);
+        cmpRefresh.setDrawingCacheBackgroundColor(R.color.c9);
+        cmpRefresh.setProgressBackgroundColorSchemeResource(R.color.c8);
+        cmpRefresh.post(new Runnable() {
+            @Override
+            public void run() {
+                cmpRefresh.setRefreshing(true);
+                Task.getInstance().exec(ListRecyclerActivity.this);
+            }
+        });
+        cmpRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                data.clear();
+                Task.getInstance().exec(ListRecyclerActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void preExec() {
+    }
+
+    @Override
+    public List<Warrior> inHandle() {
+        String jsonData = AssertUtils.getJsonData("list_recycler.json");
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Warrior>>() {
+        }.getType();
+        List<Warrior> list = gson.fromJson(jsonData, type);
+        return list;
+    }
+
+    @Override
+    public void complete(final List<Warrior> list) {
+        if (list != null && !list.isEmpty()) {
+            data.addAll(list);
+            mAdapter.notifyDataSetChanged();
+        }
+        isLoading = false;
+        cmpRefresh.setRefreshing(false);
+        mAdapter.notifyItemRemoved(mAdapter.getItemCount());
+    }
+
+
+    /**
+     * Custom item decoration
+     */
+    public class VerticalLineDecorator extends RecyclerView.ItemDecoration {
+        private int space = 0;
+
+        public VerticalLineDecorator(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            if (parent.getChildAdapterPosition(view) == 0)
+                outRect.top = space;
+
+            outRect.bottom = space;
+        }
     }
 
 }
---
+++ app/src/main/java/com/dmio/org/tut/activity/list/ListRecyclerAdapter.java
@@ -0,0 +1,103 @@
+package com.dmio.org.tut.activity.list;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.dmio.org.tut.R;
+import com.dmio.org.tut.data.model.Warrior;
+
+import java.util.List;
+
+/**
+ * 功能说明：RecyclerView对应的数据适配器<br/>
+ * 作者：wangmeng on 2017/3/29 20:23<br/>
+ * 邮箱：wangmeng@pycredit.cn
+ */
+
+public class ListRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private List<Warrior> data;
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_FOOTER = 1;
+
+    public void setData(List<Warrior> data) {
+        this.data = data;
+    }
+
+    public ListRecyclerAdapter(Context context, List<Warrior> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_refresh_recycler_view_item, parent, false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_list_recycler_view_footer, parent, false);
+            return new FooterViewHolder(view);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        if (viewType == TYPE_ITEM && holder instanceof ItemViewHolder) {
+            ItemViewHolder viewHolder = (ItemViewHolder) holder;
+            Warrior warrior = data.get(position);
+            viewHolder.bindData(warrior);
+        } else if (viewType == TYPE_FOOTER) {
+            //do nothing
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (this.data != null && !this.data.isEmpty()) {
+            return data.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position + 1 == getItemCount()) {
+            return TYPE_FOOTER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    public final class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView tvDesc;
+        private final ImageView ivPortrait;
+
+        public ItemViewHolder(View view) {
+            super(view);
+            tvDesc = (TextView) view.findViewById(R.id.tv_desc);
+            ivPortrait = (ImageView) view.findViewById(R.id.iv_portrait);
+        }
+
+        public void bindData(Warrior warrior) {
+            tvDesc.setText(warrior.getDesc());
+            Glide.with(context).load(warrior.getImgUrl()).crossFade().into(ivPortrait);
+        }
+    }
+
+    public final class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
